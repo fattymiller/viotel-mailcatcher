0ed5c22288c85ca7da999ef60ebeda206950e212
Fatty <fattymiller@gmail.com>
2022-10-31 06:10:50 +1000

    class method for services

--- lib/services/email_to_sms_service.rb
+++ lib/services/email_to_sms_service.rb
@@ -2,20 +2,22 @@ require 'mail_catcher/bus'
 require 'net/http'
 require 'uri'
 
-module EmailToSmsService
-  def publish(message)
-    message[:recipients].each do |recipient|
-      headers = { 'user' => MailCatcher.options[:sms_api_user], 'Api-Key' => MailCatcher.options[:sms_api_key] }
-      body = {
-        'sender' => MailCatcher.options[:sms_api_sender],
-        'to' => recipient,
-        'message' => Mail.new(message[:source]).decoded,
-        'test' => true
-      }
+class EmailToSmsService
+  class << self
+    def publish(message)
+      message[:recipients].each do |recipient|
+        headers = { 'user' => MailCatcher.options[:sms_api_user], 'Api-Key' => MailCatcher.options[:sms_api_key] }
+        body = {
+          'sender' => MailCatcher.options[:sms_api_sender],
+          'to' => recipient,
+          'message' => Mail.new(message[:source]).decoded,
+          'test' => true
+        }
 
-      puts "Relaying to '#{recipient}'"
+        puts "Relaying to '#{recipient}'"
 
-      Net::HTTP.post_form(URI('https://www.5centsms.com.au/api/v4/sms'), body, headers)
+        Net::HTTP.post_form(URI('https://www.5centsms.com.au/api/v4/sms'), body, headers)
+      end
     end
   end
 end
